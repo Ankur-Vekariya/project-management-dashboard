2bf762f95afcf1f674688ee8cfbcac6ac2817f6b
Ankur-Vekariya <ankurvekariya2001@gmail.com>
2023-11-01 23:49:32 +0530

    Fix css changes

--- src/sections/projects/view/projects.jsx
+++ src/sections/projects/view/projects.jsx
@@ -3,8 +3,18 @@ import React, { useState, useEffect } from 'react';
 
 import Card from '@mui/material/Card';
 import { alpha } from '@mui/material/styles';
+import { deepOrange } from '@mui/material/colors';
 import CardContent from '@mui/material/CardContent';
-import { Box, Stack, Button, Container, Typography } from '@mui/material';
+import {
+  Box,
+  Grid,
+  Stack,
+  Button,
+  Avatar,
+  Container,
+  Typography,
+  AvatarGroup,
+} from '@mui/material';
 
 import { useRouter } from 'src/routes/hooks';
 
@@ -47,27 +57,48 @@ export default function ProjectsView() {
       <Box
         sx={{
           py: 2,
-          px: 2.5,
+          px: 2,
           borderRadius: 1.5,
-          bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
+          bgcolor: (theme) => alpha(theme.palette.grey[500], 0.1),
+          display: 'flex',
         }}
       >
-        {projects.map((item, index) => (
-          <Card sx={{ width: 275, my: 2, mx: 2.5 }} key={index}>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="div">
-                adjective
-                {/* be{bull}nev{bull}o{bull}lent */}
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-              </Typography>
-            </CardContent>
-          </Card>
-        ))}
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {projects.map((item, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <Card sx={{ minWidth: 100, boxShadow: 20 }} key={index}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {item?.projectName}
+                  </Typography>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    {item?.description}
+                  </Typography>
+
+                  <Typography color="text.secondary"> {item?.technology}</Typography>
+                  <AvatarGroup max={4} sx={{}}>
+                    <Avatar
+                      sx={{ bgcolor: deepOrange[500] }}
+                      alt="Remy Sharp"
+                      src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fflat-business-man-user-profile-avatar-icon-vector-4333097&psig=AOvVaw1eEJ5u7ldrd7BCKlTXCXWy&ust=1698948454014000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMjmq6uyo4IDFQAAAAAdAAAAABAR.jpg"
+                    />
+                    <Avatar
+                      alt="Travis Howard"
+                      src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Duser&psig=AOvVaw1eEJ5u7ldrd7BCKlTXCXWy&ust=1698948454014000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMjmq6uyo4IDFQAAAAAdAAAAABAE"
+                    />
+                    <Avatar
+                      alt="Cindy Baker"
+                      src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fuser-avatar&psig=AOvVaw2iRgJbVWGtPq8dTpYEuwDP&ust=1698948568489000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMCzh-Kyo4IDFQAAAAAdAAAAABAE"
+                    />
+                    <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+                    <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+                  </AvatarGroup>
+                  {/* </Box> */}
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </Container>
   );
